dab0693bc9f857769b4fcbee6c85748651414e67
Nikhil Yadav <159103944+nikhilny1603@users.noreply.github.com>
2025-02-19 18:17:56 +0530

    Add files via upload

--- FileHandling.java
+++ FileHandling.java
@@ -1,26 +1,28 @@
+import java.io.File;
 import java.io.*;
-
+import java.util.Scanner;
 class FileHandling{
-	public static void main(String []args){
-		File f=new File("D:\\133\\NewFile.txt");
-try{
-	if(f.createNewFile()){
-		System.out.println("File is created successfully.");
-	}
-	else{
-		System.out.println("File already exists.");
-	}
-	FileReader fr=new FileReader(f);
-	BufferedReader bf=new BufferedReader(fr);
-	String line;
-	while((line = bf.readLine())!=null){
-		System.out.println(line);
+    public static void main(String []args){
+        try{
+            File f=new File("Database.txt");
+            if(f.createNewFile()){
+                System.out.println("File Created "+ f.getName());
+            }
+            else {
+                System.out.println("File Already Exists.");
+            }
+            FileWriter Writer = new FileWriter("Database.txt");
+            Writer.write("Hello Everyone. How Are You ? ");
+            Writer.close();
+	    Scanner r = new Scanner(f);
+		while(r.hasNextLine()){
+			String data =  r.nextLine();
+			System.out.println(data);
+            
+        	}
 }
-	bf.close();
-	fr.close();
-}
-catch(java.lang.Exception e){
-		System.out.println("An error occured."+ e.getMessage());
-	}
-	}
+        catch(java.lang.Exception e){
+            System.out.println("File Handling Error Occured.");
+        }
+    }
 }
