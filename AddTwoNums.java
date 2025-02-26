de1d3db4968bd4f0f9e748e4d5576f1cd8e07abc
Nikhil Yadav <159103944+nikhilny1603@users.noreply.github.com>
2025-02-26 16:58:19 +0530

    Add files via upload

---
+++ AddTwoNums.java
@@ -0,0 +1,18 @@
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
+class AddTwoNums{
+    public ListNode Add(ListNode l1,ListNode l2){
+
+
+    }
+    public static void main(String []args){
+
+    }
+}
