181f0da25110e92b3cf0156104c4ee54823c1d79
Nikhil Yadav <159103944+nikhilny1603@users.noreply.github.com>
2025-02-27 18:49:31 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.Date.*;
+class Date{
+    public static void main(String args[]){
+        java.util.Date TodayDate = new java.util.Date();
+        System.out.println("Date : "+ TodayDate);
+        System.out.println("TodayDate : "  + TodayDate.getTime());
+    }
+}
